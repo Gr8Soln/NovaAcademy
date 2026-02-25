6701f181782ce0585fd1983b6c0d72339096ff91
gr8soln <emmanuelishola7@gmail.com>
2026-02-25 08:41:11 +0100

    Add Vercel configuration for URL rewrites

--- ui/src/components/ui/logo.tsx
+++ ui/src/components/ui/logo.tsx
@@ -1,25 +1,39 @@
 import { Link } from "react-router-dom";
-import { BookOpen, GraduationCap } from "lucide-react";
 
-import { cn } from "@/lib/utils";
 import { displayName, pages } from "@/lib/constant";
+import { cn } from "@/lib/utils";
 
 export const LogoWithName = ({
-    to = pages.home,
-    isWhite = false,
+  to = pages.home,
+  isWhite = false,
 }: {
-    to?: string;
-    isWhite?: boolean;
+  to?: string;
+  isWhite?: boolean;
 }) => {
-    return (
-        <Link to={to} className={cn("inline-flex items-center gap-2", isWhite ? "text-white" : "text-primary-700")}>
-            <div className={cn("flex items-center gap-1", isWhite ? "text-white" : "text-primary-700")}>
-                <BookOpen className="hidden h-5 w-5" />
-                <GraduationCap className="h-6 w-6" />
-            </div>
-            <span className={cn("font-display text-xl font-bold", isWhite ? "text-white" : "text-primary-700")}>
-                {displayName}
-            </span>
-        </Link>
-    )
-}
+  return (
+    <Link
+      to={to}
+      className={cn(
+        "inline-flex items-center gap-2",
+        isWhite ? "text-white" : "text-primary-700",
+      )}
+    >
+      <div
+        className={cn(
+          "flex items-center gap-1",
+          isWhite ? "text-white" : "text-primary-700",
+        )}
+      >
+        <img src="/logo.png" className="h-6 w-6" alt="Nova Academy Logo" />
+      </div>
+      <span
+        className={cn(
+          "font-display text-xl font-bold",
+          isWhite ? "text-white" : "text-primary-700",
+        )}
+      >
+        {displayName}
+      </span>
+    </Link>
+  );
+};
